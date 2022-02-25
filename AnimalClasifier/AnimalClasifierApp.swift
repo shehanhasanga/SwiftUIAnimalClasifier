3529ceed826a02dfe56d88f0a4c3cd2d29831c21
shehanhasanga <shehanhasanga.15@cse.mrt.ac.lk>
2022-02-25 11:29:20 +0530

    Initial Commit

---
+++ AnimalClasifier/AnimalClasifierApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimalClasifierApp.swift
+//  AnimalClasifier
+//
+//  Created by shehan karunarathna on 2022-02-25.
+//
+
+import SwiftUI
+
+@main
+struct AnimalClasifierApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
